c6e8151ba3519f0c1247b07aff23009a9e4be395
leocassiosilva <leocassiosilva1234@gmail.com>
2022-02-28 19:48:00 -0300

    add coisas do react js

--- frontend/src/ItemComponent.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default function ItemComponent(props){
-    const status = props.status;
-    return <li>{ props.name } 
-        Status: {status ? <span>Finalizado</span> : <span>Nao Finalizado</span>}
-    </li>
-}
