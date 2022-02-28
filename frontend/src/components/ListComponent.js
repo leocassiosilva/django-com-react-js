c56e52d5282d7a2c5ab285029a9cbae3c38fbe3c
leocassiosilva <leocassiosilva1234@gmail.com>
2022-02-28 20:29:51 -0300

    component

---
+++ frontend/src/components/ListComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export  default function ListComponent(){
+    return (
+       <div>
+            <h2>Minha Lista</h2>
+            <ul>
+                <li>Item 1</li>
+                <li>Item 1</li>
+            </ul>
+       </div>
+    );
+}
