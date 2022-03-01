a01252932247bcb53c6ba1283d30ed36f6b56b46
leocassiosilva <leocassiosilva1234@gmail.com>
2022-03-01 18:03:46 -0300

    listando as listas dos user

--- frontend/src/components/ItemComponent.js
+++ frontend/src/components/ItemComponent.js
@@ -3,5 +3,5 @@ import React from 'react';
 export  default function ItemComponent(props){
     const status = props.status;
     return <li>{props.name} 
-    <p>Status:{status ? <div>Finalizado!</div> : <div>Não Finalizado!</div>}</p></li>
+    <div>Status:{status ? <p>Finalizado!</p> : <p>Não Finalizado!</p>}</div></li>
 }
